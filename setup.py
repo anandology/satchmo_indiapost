c4b8a54316251bab6a6f2d2b85e190244e2285b0
Anand Chitipothu <anandology@gmail.com>
2011-12-20 10:21:42 +0530

    Added url to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='Satchmo extension to support shipping by India Post',
     author="Anand Chitipothu",
     author_email="anandology@gmail.com",
+    url='https://github.com/anandology/satchmo_indiapost',
     packages=["satchmo_indiapost"]
 )
 
